ba91e67f408f7965413786a0c0e2e92ec7d31cc0
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2021-12-01 15:41:56 +0700

    fixed constants

--- recog_joy_algo/constants.py
+++ recog_joy_algo/constants.py
@@ -3,24 +3,47 @@ import json
 json_file = open("constants.json")
 constants = json.load(json_file)
 
+# MQTT CONSTANTS
 mqtt = constants["MQTT"]
 mqtt_username = mqtt["username"]
 mqtt_password = mqtt["password"]
 mqtt_topic = mqtt["topic"]
 mqtt_ip = mqtt["ip"]
 
+# MYSQL CONSTANTS
 mysql = constants["MySQL"]
 mysql_username = mysql["username"]
 mysql_password = mysql["password"]
 mysql_ip = mysql["ip"]
 
+# SSC32 CONSTANTS
 ssc32 = constants["SSC32"]
 ssc32_mode = ssc32["mode"]
 
+# OPENCV CONSTANTS
 cv = constants["OpenCV"]
+
+cv_framerate = cv["frame_rate"]
+
 cv_x_dim = cv["x_dim"]
 cv_y_dim = cv["y_dim"]
+cv_resolution = (cv_x_dim, cv_y_dim)
+
+cv_half_x_dim = cv_x_dim // 2
+cv_half_y_dim = cv_y_dim // 2
+
+cv_lower_bound = cv_half_x_dim - cv_half_x_dim // 2
+cv_upper_bound = cv_half_x_dim + cv_half_x_dim // 2
+
+cv_hard_lower_bound = cv_half_x_dim - cv_half_x_dim // 5
+cv_hard_upper_bound = cv_half_x_dim + cv_half_x_dim // 5
+
 cv_max_pixel = cv["max_pixel"]
+cv_max_frame_difference = cv_max_pixel * cv_x_dim * cv_y_dim
+
+cv_vector_min_x = cv_x_dim // 10
+cv_vector_min_y = cv_y_dim // 10
+
 cv_canny_min = cv["canny_min"]
 cv_canny_max = cv["canny_max"]
 cv_prev_c_x = cv["prev_c_x"]
@@ -29,56 +52,9 @@ cv_frames_before_refresh = cv["frames_before_refresh"]
 cv_magnitude_lower_boundary = cv["magnitude_lower_boundary"]
 cv_magnitude_upper_boundary = cv["magnitude_upper_boundary"]
 
+# CONVEYOR BELT CONSTANTS
 conveyor = constants["ConveyorBelt"]
 conveyor_pwm_pin = conveyor["PWM_pin"]
 conveyor_high_dc = conveyor["high_dc"]
 conveyor_low_dc = conveyor["low_dc"]
 conveyor_stop_dc = conveyor["stop_dc"]
-
-mqtt_username = constants["MQTT"]["username"]
-password = "191900103"
-topic = "channel"
-
-broker_ip = "10.252.243.158"
-
-db_host_ip = "10.252.242.117"
-mode = 1
-
-ch1_prev = 0
-ch2_prev = 0
-ch3_prev = 0
-ch4_prev = 0
-
-controlpin = 14
-
-dc = 90
-
-cycle_count = 0
-frame_count = 0
-reset_count = 0
-
-x_dim = 640
-y_dim = 480
-
-half_x_dim = int(x_dim / 2)
-half_y_dim = int(y_dim / 2)
-
-lower_bound = half_x_dim - int(x_dim / 4)
-upper_bound = half_x_dim + int(x_dim / 4)
-lower_bound_hard = half_x_dim - int(x_dim / 10)
-upper_bound_hard = half_x_dim + int(x_dim / 10)
-max_frame_difference = 255 * x_dim * y_dim
-
-canny_min = 100
-canny_max = 120
-
-prev_c_x, prev_c_y = 0, 180
-c_x, c_y = 0, 180
-
-vector_min_x = int(x_dim / 10)
-vector_min_y = int(y_dim / 10)
-
-frames_before_refresh = 3
-
-magnitude_threshold = 0.004
-max_magnitude_threshold = 0.025
--- recog_joy_algo/picam.py
+++ recog_joy_algo/picam.py
@@ -10,58 +10,11 @@ import constants
 
 dbconn = DBConnection()
 
-username = "jm03"
-password = "191900103"
-topic = "channel"
-
-broker_ip = "10.252.243.158"
-
-mode = 1
-
-ch1_prev = 0
-ch2_prev = 0
-ch3_prev = 0
-ch4_prev = 0
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-controlpin = 14
-
-GPIO.setup(controlpin, GPIO.OUT)
-pwm = GPIO.PWM(controlpin, 100)
-
-dc = 90
-
-frame_count = 0
-reset_count = 0
-
-x_dim = 640
-y_dim = 480
-
-half_x_dim = int(x_dim / 2)
-half_y_dim = int(y_dim / 2)
-
-lower_bound = half_x_dim - int(x_dim / 4)
-upper_bound = half_x_dim + int(x_dim / 4)
-lower_bound_hard = half_x_dim - int(x_dim / 10)
-upper_bound_hard = half_x_dim + int(x_dim / 10)
-max_frame_difference = 255 * x_dim * y_dim
-
-canny_min = 100
-canny_max = 120
-
-prev_c_x, prev_c_y = 0, 180
-c_x, c_y = 0, 180
-
-vector_min_x = int(x_dim / 10)
-vector_min_y = int(y_dim / 10)
-
-frames_before_refresh = 3
-
-magnitude_threshold = 0.004
-max_magnitude_threshold = 0.025
-
+GPIO.setup(constants.conveyor_pwm_pin, GPIO.OUT)
+pwm = GPIO.PWM(constants.conveyor_pwm_pin, 100)
 prev_img = []
 
 
@@ -70,17 +23,24 @@ def matrix_sum(mat):
     y_size = len(mat)
     for i in range(y_size):
         arrsum += sum(mat[i])
-    return arrsum / max_frame_difference
+    return arrsum / constants.cv_max_frame_difference
 
 
 camera = PiCamera()
-camera.resolution = (x_dim, y_dim)
-camera.framerate = 10
-rawcap = PiRGBArray(camera, size=(x_dim, y_dim))
+camera.resolution = constants.cv_resolution
+camera.framerate = constants.cv_framerate
+rawcap = PiRGBArray(camera, size=camera.resolution)
 time.sleep(0.5)
 
+current_dc = constants.conveyor_high_dc
+frame_count = 0
+reset_count = 0
+
+prev_c_x, prev_c_y = 0, 0
+c_x, c_y = 0, 0
 for frame in camera.capture_continuous(rawcap, format="bgr", use_video_port=True):
-    pwm.start(dc)
+
+    pwm.start(current_dc)
     frame_count += 1
     if frame_count <= 1:
         prev_img = Frame(frame.array)
@@ -88,14 +48,14 @@ for frame in camera.capture_continuous(rawcap, format="bgr", use_video_port=True
     img = Frame(frame.array)
 
     diff = cv2.subtract(img.blur(2), prev_img.blur(2))
-    canny_diff = cv2.Canny(diff, canny_min, canny_max)
+    canny_diff = cv2.Canny(diff, constants.cv_canny_min, constants.cv_canny_max)
     t_res, thresh = cv2.threshold(canny_diff, 127, 255, 0)
 
     thresh_mag = matrix_sum(thresh)
 
     moments = cv2.moments(thresh)
 
-    if magnitude_threshold < thresh_mag < max_magnitude_threshold:
+    if constants.cv_magnitude_lower_boundary < thresh_mag < constants.cv_magnitude_upper_boundary:
         if moments["m00"] != 0:
             c_x = int(moments["m10"] / moments["m00"])
             c_y = int(moments["m01"] / moments["m00"])
@@ -103,19 +63,26 @@ for frame in camera.capture_continuous(rawcap, format="bgr", use_video_port=True
     centroid_vec_x = c_x - prev_c_x
     centroid_vec_y = c_y - prev_c_y
 
-    if abs(centroid_vec_x) < vector_min_x:
+    if abs(centroid_vec_x) < constants.cv_vector_min_x:
         centroid_vec_x = 0
-    if abs(centroid_vec_y) < vector_min_y:
+
+    if abs(centroid_vec_y) < constants.cv_vector_min_y:
         centroid_vec_y = 0
+
     print(" > Mvmnt Mag:", thresh_mag)
-    print(lower_bound, lower_bound_hard, c_x, upper_bound_hard, upper_bound)
+    print(constants.cv_lower_bound,
+          constants.cv_hard_lower_bound,
+          c_x,
+          constants.cv_hard_upper_bound,
+          constants.cv_upper_bound)
+
     if reset_count > 1:
-        if lower_bound < c_x < upper_bound:
+        if constants.cv_lower_bound < c_x < constants.cv_upper_bound:
             print(" > set dc to 60")
-            dc = 60
+            current_dc = constants.conveyor_low_dc
             print("  >> close to center")
-            if lower_bound_hard < c_x < upper_bound_hard:
-                dc = 0
+            if constants.cv_hard_lower_bound < c_x < constants.cv_hard_upper_bound:
+                current_dc = constants.conveyor_stop_dc
                 dbconn.execute_commit("")
                 print("  >> at center")
                 print(" >>> picking up item...")
@@ -128,17 +95,20 @@ for frame in camera.capture_continuous(rawcap, format="bgr", use_video_port=True
 
         else:
             print(" > set dc to 80")
-            dc = 90
-    if frame_count % frames_before_refresh == 0:
+            current_dc = constants.conveyor_high_dc
+
+    if frame_count % constants.cv_frames_before_refresh == 0:
         print("--reset")
         reset_count += 1
         prev_img = Frame(frame.array)
         c_x, c_y = 0, 0
-    cv2.circle(thresh, (c_x, half_y_dim), 5, (255, 255, 255), -1)
+
+    cv2.circle(thresh, (c_x, constants.cv_half_y_dim), 5, (255, 255, 255), -1)
     cv2.imshow("Difference Frame", thresh)
     prev_c_x, prev_c_y = c_x, c_y
 
     rawcap.truncate(0)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
