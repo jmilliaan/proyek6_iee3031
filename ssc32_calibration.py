b8f16480b6405f758ba6ecdae7b4a2c0bd47cde3
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2021-12-07 13:39:34 +0700

    changed preparation duration

--- ssc32_calibration.py
+++ ssc32_calibration.py
@@ -6,8 +6,9 @@ ssc = SSC32RoboticArm(c.ssc32_serial_port, c.ssc32_baud_rate)
 
 while True:
     ssc.reset_ready(1)
+    time.sleep(2)
     ssc.grab_position()
-    time.sleep(1)
+    time.sleep(2)
     ssc.drop_position()
-    time.sleep(1)
+    time.sleep(2)
     print("cycle done")
