3f3a59e8ea0e63e2c198f942335e059ad602db78
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2021-12-07 15:50:59 +0700

    changed preparation duration

---
+++ roboconvcv_top.py
@@ -0,0 +1,89 @@
+import time
+import cv2
+from roboconvcv_imgframe import Frame, PiCam
+from roboconvcv_database import DBConnection
+from roboconvcv_ssc32 import SSC32RoboticArm
+from roboconvcv_conveyor import ConveyorBelt
+import roboconvcv_constants as constants
+
+frame_count = 0
+reset_count = 0
+
+prev_img = []
+
+prev_c_x, prev_c_y = 0, 0
+c_x, c_y = 0, 0
+
+if __name__ == '__main__':
+    db = DBConnection()
+    robo_arm = SSC32RoboticArm(constants.ssc32_serial_port,
+                               constants.ssc32_baud_rate)
+    conveyor = ConveyorBelt()
+    camera = PiCam()
+    conveyor.start()
+
+    robo_arm.reset_ready(2)
+
+    db.log_start_conv()
+
+    for frame in camera.cam.capture_continuous(camera.raw_cap,
+                                               format="bgr",
+                                               use_video_port=True):
+        img = Frame(frame.array)
+
+        print(f"FRAME: {frame_count}")
+        print(" > Movement Magnitude:", img.frame_magnitude)
+        print(constants.cv_lower_bound,
+              constants.cv_hard_lower_bound,
+              c_x,
+              constants.cv_hard_upper_bound,
+              constants.cv_upper_bound)
+
+        if frame_count <= 1:
+            prev_img = Frame(frame.array)
+
+        canny_diff = img.canny_difference(prev_img)
+        c_x, c_y = img.centroid(canny_diff)
+
+        cv2.circle(canny_diff, (c_x, c_y), 5, (255, 255, 255), -1)
+        cv2.imshow("Difference Frame", canny_diff)
+        if reset_count > 1:
+            if constants.cv_lower_bound < c_x < constants.cv_upper_bound:
+                conveyor.change_dc(constants.conveyor_low_dc)
+                db.log_slow_conv()
+                print(" >> close to center")
+
+                if constants.cv_hard_lower_bound < c_x < constants.cv_hard_upper_bound:
+                    conveyor.change_dc(constants.conveyor_stop_dc)
+                    db.log_stop_conv()
+                    db.log_ssc_take_item()
+                    print(" >>> at center", end=" ")
+                    print(constants.cv_hard_lower_bound,
+                          c_x,
+                          constants.cv_hard_upper_bound)
+                    time.sleep(1)
+                    robo_arm.grab_drop_ready(1)
+                    img.frame_magnitude = 0
+                    camera.raw_cap.truncate(0)
+                    c_x, c_y = 0, 0
+                    time.sleep(1)
+                    continue
+
+            else:
+                conveyor.change_dc(constants.conveyor_high_dc)
+                db.log_start_conv()
+
+        if frame_count % constants.cv_frames_before_refresh == 0:
+            reset_count += 1
+            prev_img = Frame(frame.array)
+
+        prev_c_x, prev_c_y = c_x, c_y
+
+        camera.raw_cap.truncate(0)
+        frame_count += 1
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+    exit()
