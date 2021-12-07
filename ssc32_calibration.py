b8509295b4575af025f177cf0cbe8e2625225209
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2021-12-07 13:38:13 +0700

    changed preparation duration

---
+++ ssc32_calibration.py
@@ -0,0 +1,13 @@
+import time
+import roboconvcv_constants as c
+from roboconvcv_ssc32 import SSC32RoboticArm
+
+ssc = SSC32RoboticArm(c.ssc32_serial_port, c.ssc32_baud_rate)
+
+while True:
+    ssc.reset_ready(1)
+    ssc.grab_position()
+    time.sleep(1)
+    ssc.drop_position()
+    time.sleep(1)
+    print("cycle done")
