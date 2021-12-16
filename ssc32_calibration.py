290e4ae14b756bc7bb9c25616ca6ebadd9ff9062
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2021-12-16 11:55:42 +0700

    database connected

--- ssc32_calibration.py
+++ ssc32_calibration.py
@@ -2,6 +2,6 @@ from roboconvcv_database import DBConnection
 
 db = DBConnection()
 
-db.execute_commit("INSERT INTO command(command, sequence) VALUES(0, 1)")
+db.execute_commit("INSERT INTO command(command, sequence) VALUES(1, 1)")
 
 db.execute_commit("INSERT INTO command(command, sequence) VALUES(1, 2)")
