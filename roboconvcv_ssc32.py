bec9501308b7f9e9a82800f440b35c68e1de493c
Joy Milliaan <69390398+jmilliaan@users.noreply.github.com>
2021-12-16 16:18:54 +0700

    Update roboconvcv_ssc32.py

--- roboconvcv_ssc32.py
+++ roboconvcv_ssc32.py
@@ -11,69 +11,69 @@ class SSC32RoboticArm:
         self.serial_port = port
         self.baud_rate = baud
         self.connected = False
+#         try:
+#             self.ser = serial.Serial(self.serial_port, self.baud_rate, timeout=1)
+#             print(f"Connected to {self.serial_port, self.ser}")
+#             self.connected = True
+#         except serial.serialutil.SerialException:
+#             self.connected = False
+#             print(f"Serial port {self.serial_port} not found")
+#             pass
         try:
-            self.ser = serial.Serial(self.serial_port, self.baud_rate, timeout=1)
-            print(f"Connected to {self.serial_port, self.ser}")
+            self.ssc = ssc32.SSC32(self.serial_port, self.baud_rate)
             self.connected = True
         except serial.serialutil.SerialException:
+            print(f"Port {self.serial_port} not found.")
             self.connected = False
-            print(f"Serial port {self.serial_port} not found")
             pass
-        # try:
-        #     self.ssc = ssc32.SSC32(self.serial_port, self.baud_rate)
-        #     self.connected = True
-        # except serial.serialutil.SerialException:
-        #     print(f"Port {self.serial_port} not found.")
-        #     self.connected = False
-        #     pass
 
     def move_ssc(self, servo, pos, dur):
         if self.connected:
-            self.ser.write(bytes(f"#{servo} P{pos} T{dur} <cr>", "utf-8"))
-            # self.ssc.commit(time=dur)
-            # self.ssc[servo].position = pos
+#             self.ser.write(bytes(f"#{servo} P{pos} T{dur} <cr>", "utf-8"))
+            self.ssc.commit(time=dur)
+            self.ssc[servo].position = pos
 
     def reset_position(self):
         if self.connected:
             print("||| SSC32 RESET POSITION |||")
             self.move_ssc(0, 1500, self.extended_dur)
-            # self.move_ssc(1, 1500, self.extended_dur)
-            # self.move_ssc(2, 1500, self.extended_dur)
-            # self.move_ssc(3, 1500, self.extended_dur)
-            # self.move_ssc(4, 1000, self.extended_dur)
+            self.move_ssc(1, 1500, self.extended_dur)
+            self.move_ssc(2, 1500, self.extended_dur)
+            self.move_ssc(3, 1500, self.extended_dur)
+            self.move_ssc(4, 1000, self.extended_dur)
 
     def ready_position(self):
         if self.connected:
             print("||| SSC32 READY POSITION |||")
             self.move_ssc(0, 2400, self.extended_dur)
-            # self.move_ssc(1, 1600, self.extended_dur)
-            # self.move_ssc(2, 1200, self.extended_dur)
-            # self.move_ssc(3, 1350, self.extended_dur)
-            # self.move_ssc(4, 2400, self.extended_dur)
+            self.move_ssc(1, 1600, self.extended_dur)
+            self.move_ssc(2, 1200, self.extended_dur)
+            self.move_ssc(3, 1350, self.extended_dur)
+            self.move_ssc(4, 2400, self.extended_dur)
 
     def grab_position(self):
         if self.connected:
             print("||| SSC32 GRAB POSITION |||")
             self.move_ssc(0, 2400, self.default_dur)
-            # self.move_ssc(1, 1400, self.default_dur)
-            # self.move_ssc(2, 1200, self.default_dur)
-            # self.move_ssc(3, 1150, self.default_dur)
-            # time.sleep(self.default_dur / 1000)
-            # self.move_ssc(4, 1000, self.default_dur)
-            # time.sleep(2 * self.default_dur / 1000)
-            # self.move_ssc(4, 2400, self.default_dur)
+            self.move_ssc(1, 1400, self.default_dur)
+            self.move_ssc(2, 1200, self.default_dur)
+            self.move_ssc(3, 1150, self.default_dur)
+            time.sleep(self.default_dur / 1000)
+            self.move_ssc(4, 1000, self.default_dur)
+            time.sleep(2 * self.default_dur / 1000)
+            self.move_ssc(4, 2400, self.default_dur)
 
     def drop_position(self):
         if self.connected:
             print("||| SSC32 DROP POSITION |||")
             self.move_ssc(0, 1500, self.default_dur)
-            # self.move_ssc(1, 1700, self.default_dur)
-            # self.move_ssc(2, 1300, self.default_dur)
-            # self.move_ssc(3, 1350, self.default_dur)
-            # time.sleep(self.default_dur / 1000)
-            # self.move_ssc(4, 2400, self.default_dur)
-            # time.sleep(self.default_dur / 1000)
-            # self.move_ssc(4, 1000, self.default_dur)
+            self.move_ssc(1, 1700, self.default_dur)
+            self.move_ssc(2, 1300, self.default_dur)
+            self.move_ssc(3, 1350, self.default_dur)
+            time.sleep(self.default_dur / 1000)
+            self.move_ssc(4, 2400, self.default_dur)
+            time.sleep(self.default_dur / 1000)
+            self.move_ssc(4, 1000, self.default_dur)
 
     def reset_ready(self, dur):
         if self.connected:
